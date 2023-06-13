4814daeb04680f2aff404289eb5fc3963a7cdf0b
Edem <edem.muratov01@mail.ru>
2023-06-13 21:36:56 +0300

    asd

---
+++ Seminar_1/ConsoleApp6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит случайное 
+// трехзначное число и удаляет вторую цифру этого числа
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Рандомное число: {number}");
+
+int a = number / 100; 
+int b = number % 10;
+
+int result = (a * 10) + b;
+Console.Write(result);
