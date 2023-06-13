6c0164b5cd089802bb86f48205f0aa3061fecf83
Edem <edem.muratov01@mail.ru>
2023-06-13 20:54:58 +0300

    Наибольшая цифра числа

---
+++ Seminar_1/ConsoleApp5/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит случайное число из 
+// отрезка [10, 99] и показывает наибольшую цифру числа
+
+int number = new Random().Next(10, 100);  // Случайное число от 10 до 99
+Console.WriteLine($"Рандомное число: {number}");
+
+int FirstDigit = number / 10;
+int SecondDigit = number % 10;
+
+if ( FirstDigit > SecondDigit)
+{
+    Console.WriteLine($"Наибольшая цифра в числе: {FirstDigit}");
+}
+else
+{
+    Console.WriteLine($"Наибольшая цифра в числе: {SecondDigit}");
+}
+
+
