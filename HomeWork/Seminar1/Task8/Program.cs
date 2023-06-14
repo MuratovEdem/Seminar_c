dfc7a1c661fd993c53de0065847d4bfa0752be0d
Edem <edem.muratov01@mail.ru>
2023-06-14 20:53:20 +0300

    task8

---
+++ HomeWork/Seminar1/Task8/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введи число: ");
+int n = int.Parse(Console.ReadLine());
+int count = 1;
+
+if (n > 0)
+{
+    while(count != n)
+    {
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count} ");
+    }
+    count++;
+    }
+}
+else
+{
+    while(count != n)
+    {
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count} ");
+    }
+    count--;
+    }
+}
