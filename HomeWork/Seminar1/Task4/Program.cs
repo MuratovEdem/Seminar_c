e5ea62dd3515052713b684f62c4d449acf2899ff
Edem <edem.muratov01@mail.ru>
2023-06-14 20:27:04 +0300

    task4

---
+++ HomeWork/Seminar1/Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+
+Console.Write("Введи первое число: ");
+int FirstDigit = int.Parse(Console.ReadLine());
+
+Console.Write("Введи второе число: ");
+int SecondDigit = int.Parse(Console.ReadLine());
+
+Console.Write("Введи третье число: ");
+int ThirdDigit = int.Parse(Console.ReadLine());
+
+int max = FirstDigit;
+
+if (SecondDigit > max)
+{
+    max = SecondDigit;
+}
+if (ThirdDigit > max)
+{
+    max = ThirdDigit;
+}
+Console.Write($"Наибольшее число равно {max}");
