38894ea3fa91c8ba1ea2628d9bc10c9b6daf104b
Edem <edem.muratov01@mail.ru>
2023-06-16 22:00:23 +0300

    Task 22

--- Seminar_1/ConsoleApp14/Program.cs
+++ Seminar_1/ConsoleApp14/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и 
+//выдаёт таблицу квадратов чисел от 1 до N.
+
+double pow_a(int count)
+{
+    double result = Math.Pow(count, 2);
+    return result;
+}
+
+Console.Write("Ведите число: ");
+int n = int.Parse(Console.ReadLine());
+
+int count = 1;
+
+while (count <= n)
+{
+    double result = pow_a(count);
+    Console.WriteLine($"{result} ");
+    count++;
+}
