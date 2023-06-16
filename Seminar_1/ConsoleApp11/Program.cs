014eb3e336327061ee41643e1d7dfab0ab2bdc0c
Edem <edem.muratov01@mail.ru>
2023-06-16 21:36:42 +0300

    21

---
+++ Seminar_1/ConsoleApp11/Program.cs
@@ -0,0 +1,59 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, 
+//в которой находится эта точка. 
+
+
+int GetAreaNumber(int x, int y)
+{
+    
+}
+
+
+int GetSum(int a, int b)
+{
+    int result = a + b;
+    return result;
+}
+
+void PrintMessageToConsole(string message)
+{
+    Console.WriteLine(message);
+}
+
+PrintMessageToConsole("Hello world");
+
+GetSum(10, 15);
+
+
+/*Console.Write("Ведите координату Х: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.Write("Ведите координату Y: ");
+int y = int.Parse(Console.ReadLine());
+
+if (x == 0 || y == 0)
+{
+    Console.Write("Недопустимые координаты: X и Y != 0");
+    Environment.Exit(0); //return;
+}
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("Номер четверти: 1");
+}
+
+if (x < 0 && y > 0)
+{
+    Console.WriteLine("Номер четверти: 2");
+}
+
+if (x < 0 && y < 0)
+{
+    Console.WriteLine("Номер четверти: 3");
+}
+
+if (x > 0 && y < 0)
+{
+    Console.WriteLine("Номер четверти: 4");
+}*/
+
---
+++ Seminar_1/ConsoleApp12/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Write("Ведите номер четверти: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a < 1 || a > 4)
+{
+    Console.Write("Недопустимое значение");
+    Environment.Exit(0);
+}
+ switch (a)
+{
+     case (1):
+        Console.Write("Диапазон значений x > 0 и y > 0");
+        break;
+    case (2):
+        Console.Write("Диапазон значений x < 0 и y > 0");
+        break;
+    case (3):
+        Console.Write("Диапазон значений x < 0 и y < 0");
+        break;
+    case (4):
+        Console.Write("Диапазон значений x > 0 и y < 0");
+        break;
+}
