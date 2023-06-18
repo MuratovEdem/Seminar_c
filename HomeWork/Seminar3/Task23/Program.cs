b82f0737cd69a4a33873646fc1059e01335ec6d2
Edem <edem.muratov01@mail.ru>
2023-06-18 13:32:19 +0300

    Task23

---
+++ HomeWork/Seminar3/Task23/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+double cubing(int number)
+{
+    double result = Math.Pow(number, 3);
+    return result;
+}
+
+Console.Write("Ведите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int count = 1;
+
+while (count <= number)
+{
+    double result = cubing(count);
+    Console.Write($"{result} ");
+    count++;
+}
+
+
+
