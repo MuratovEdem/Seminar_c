c6660aec13131f6cfe3bdf49b4d26e84d3692f14
Edem <edem.muratov01@mail.ru>
2023-06-18 11:19:25 +0300

    12

---
+++ HomeWork/Seminar2/Task13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Ведите любое число: ");
+int number = int.Parse(Console.ReadLine());
+
+int count = 1;
+int result = 1;
+
+if (number > 99)
+{
+    while (count > 0)
+    {
+        result = (result * 10) + (number % 10);
+        count = number / 10;
+    }
+}
+else
+{
+    Console.WriteLine("число вне диапазона");
+}
+Console.WriteLine(result);
