c03522e9a33e5bcf1826b23dba1ffe20cf890e01
Edem <edem.muratov01@mail.ru>
2023-06-22 20:57:32 +0300

    Task28

---
+++ Seminar_1/ConsoleApp17/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число N и 
+// выдаёт произведение чисел от 1 до N
+
+Console.Write("Ведите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int number_1 = 1;
+for (int i = 1; i <= number; i++)
+{
+    number_1 = number_1 * i;
+}
+Console.WriteLine($"Произведение чисел равно {number_1}");
