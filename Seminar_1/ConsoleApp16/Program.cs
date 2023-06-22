00cccf2628d9e660e6d62a6a3b7a2ff72c71044a
Edem <edem.muratov01@mail.ru>
2023-06-22 20:52:25 +0300

    Task26

---
+++ Seminar_1/ConsoleApp16/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход 
+// число и выдаёт количество цифр в числе
+
+Console.Write("Ведите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int count = 0;
+if (number == 0)
+{
+    Console.WriteLine($"Количество цифр в числе 1");
+}
+else
+{
+    while (number != 0)
+    {
+        count++;
+        number /= 10;
+    }
+    Console.WriteLine($"Количество цифр в числе {count}");
+}
