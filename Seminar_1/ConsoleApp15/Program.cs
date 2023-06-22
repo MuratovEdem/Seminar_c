9d792c007c5a409d89a89f5b26d92f9f7958d59f
Edem <edem.muratov01@mail.ru>
2023-06-22 20:41:23 +0300

    Task24

--- Seminar_1/ConsoleApp15/Program.cs
+++ Seminar_1/ConsoleApp15/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (А) 
+// и выдаёт сумму чисел от 1 до А.
+
+Console.Write("Ведите число: ");
+int n = int.Parse(Console.ReadLine());
+int sum_n = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    sum_n = sum_n + i;
+}
+Console.WriteLine($"Сумма числе равна {sum_n}");
