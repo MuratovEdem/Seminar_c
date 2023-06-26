6d2a10c5ad15c73a2ffc54568c334602072609f3
Edem <edem.muratov01@mail.ru>
2023-06-26 14:59:30 +0300

    Task27

---
+++ HomeWork/Seminar4/Task27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на 
+// вход число и выдаёт сумму цифр в числе
+
+Console.Write("Ведите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int result = 0;
+
+while (number > 0)
+{
+    result = result + number % 10;
+    number = number / 10;
+}
+
+Console.WriteLine(result);
