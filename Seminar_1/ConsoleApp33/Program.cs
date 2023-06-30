cf43a4a58849aa95ae8c0e522f3af2c1c3baa1ad
Edem <edem.muratov01@mail.ru>
2023-06-30 22:18:52 +0300

    Seminar

--- Seminar_1/ConsoleApp33/Program.cs
+++ Seminar_1/ConsoleApp33/Program.cs
@@ -6,82 +6,38 @@ int rows = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введи количество столбцов: ");
 int colmns = Convert.ToInt32(Console.ReadLine());
 
-int[,] matrix = InitMatrix(rows, colmns);
+int[,] matrix = new int[4,4];
 
-Random random = new Random();
-
-
-int[,] RightRun(int rows, int colmns)
+void RandomNumber(int[,] arr)
 {
-    int[,] tmp = new int[rows, colmns];
     Random random = new Random();
-    for (int i = 0; i < rows; i++)
+    for (int i = 0; i < arr.GetLength(0); i++)
     {
-        for (int j = 0; j < colmns; j++)
+        for (int j = 0; j < arr.GetLength(1); j++)
         {
-            tmp[i,j] = random.Next(1, 11);;
+            if (arr[i, j] == 0)
+            {
+                arr[i,j] = random.Next(1, 10);
+            }
         }
     }
-    return tmp;
 }
 
-
-
-
-
-
-
-
-for (int i = 0; i < rows; i++)
+void RunMatrix(int row, int col)
 {
-    for (int j = 0; j < colms; j++)
+    if (matrix[row, col] == 0 || matrix[row, col] == matrix.GetLength(1) || matrix[row, col] == matrix.GetLength(0))
     {
-        if (j < colms || j + 1 > 0)
-        {
-            matrix[i,j] = random.Next(1, 10);
-            break;
-        }
-        if (i < rows || i + 1 > 0)
-        {
-            matrix[i,j] = random.Next(1, 10);
-            break;
-        }
-        if (j < colms || j + 1 > 0)
-        {
-            matrix[i,j] = random.Next(1, 10);
-            break;
-        }
-        if (i < rows || i + 1 > 0)
-        {
-            matrix[i,j] = random.Next(1, 10);
-            break;
-        }
+        matrix[row, col] = 1;
+        RunMatrix(row - 1, col);
+        RunMatrix(row, col - 1);
+        RunMatrix(row + 1, col);
+        RunMatrix(row, col + 1);
     }
 }
 
-
-
-
-
-
-
-
-int[,] InitMatrix(int rows, int colmns)
-{
-    int[,] tmp = new int[rows, colmns];
-    Random random = new Random();
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < colmns; j++)
-        {
-            tmp[i,j] = random.Next(1, 11);;
-        }
-    }
-    return tmp;
-}
-
-
-
+RandomNumber(matrix);
+RunMatrix(0,0);
+RandomNumber(matrix);
 
 
 
