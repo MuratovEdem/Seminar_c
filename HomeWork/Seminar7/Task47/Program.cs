9ea7df991b6768f91cae1ac0b76125b39ecbba43
Edem <edem.muratov01@mail.ru>
2023-07-05 14:13:50 +0300

    Task47

---
+++ HomeWork/Seminar7/Task47/Program.cs
@@ -0,0 +1,18 @@
+﻿// . Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double[,] array = new double[4,6];
+
+int[] arrayZero = new int [3] {10, 100, 1000};
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = Math.Round(new Random().NextDouble() * arrayZero[new Random().Next(0, arrayZero.Length)], 2);
+        Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+
