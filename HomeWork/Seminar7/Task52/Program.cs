b845efffb71806d1fe929b0a95fb8e2f6b9f46dd
Edem <edem.muratov01@mail.ru>
2023-07-05 14:26:56 +0300

    Task52

---
+++ HomeWork/Seminar7/Task52/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] array = new int[4,4];
+
+PrintArray(array);
+
+Console.WriteLine();
+
+GetArithmeticMean(array);
+
+
+void GetArithmeticMean (int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < arr.GetLength(0); j++)
+        {
+            sum = sum + arr[j,i];
+        }
+        double result = Math.Round(sum / 4, 2);
+        Console.WriteLine($"Среднее арифметическое в {i + 1} столбце равна {result}");
+    }
+}
+
+void PrintArray (int[,] arr)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1,10);
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
